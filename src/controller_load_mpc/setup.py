42e6f14fd425dfbdc5e7b4c6ab5aa9d8c29cab40
Wesley Donald <wesleydonaldnz@gmail.com>
2026-06-20 22:18:05 +1000

    234 worlds

---
+++ src/controller_load_mpc/setup.py
@@ -0,0 +1,27 @@
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'controller_load_mpc'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', glob('launch/*.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Wesley Donald',
+    maintainer_email='wesleydonaldnz2@gmail.com',
+    description='Centralized cable-suspended load MPC planner (Sun et al. 2025)',
+    license='Apache-2.0',
+    entry_points={
+        'console_scripts': [
+            # filled in once the planner node is implemented
+        ],
+    },
+)
